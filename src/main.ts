5efc725d2e250ec4c0557a4fb375a8ab68dd5504
cmmmli <thekomori1113@gmail.com>
2020-09-10 16:29:23 +0900

    feat: Add resize function

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,61 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import * as fs from 'fs';
+import * as path from 'path';
+
+import yargs = require('yargs');
+import * as gm from 'gm';
+const im = gm.subClass({ imageMagick: true });
+// import sharp from 'sharp';
+
+const argv = yargs.options({
+  f: { type: 'string', demand: true }
+}).argv;
+
+async function gmStreamResize(readStream: NodeJS.ReadableStream, writeStream: fs.WriteStream) {
+  return await new Promise((resolve) => {
+    const gmStream = im(readStream);
+
+    writeStream.on('finish', () => {
+      resolve(gmStream)
+    });
+
+    gmStream.resize(256, 256).stream().pipe(writeStream);
+  })
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+async function gmFilepathResize(originPath: string, thumbNailPath: string) {
+  return await new Promise((resolve) => {
+    im(originPath).resize(256, 256).write(thumbNailPath, (err) => {
+      if (!err) { resolve(); }
+    })
+  });
 }
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
+export async function resize() {
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+  const input = argv.f;
+  const parsedInput = path.parse(argv.f);
+
+  const originPath = input;
+  const originStream = fs.createReadStream(input);
+
+  const resizedPath = path.join(parsedInput.dir, `${parsedInput.name}_path_256${parsedInput.ext}`);
+  const resizedStream = fs.createWriteStream(path.join(parsedInput.dir, `${parsedInput.name}_stream_256${parsedInput.ext}`));
+
+  const promises = [];
+
+  promises.push(
+    gmStreamResize(originStream, resizedStream)
+  )
+  promises.push(
+    gmFilepathResize(originPath, resizedPath)
+  )
+
+  Promise.all(promises)
+    .then(() => {
+      console.log('Resize done!');
+    }).catch((err) => {
+      console.error(err);
+    })
 }
+
+resize();
